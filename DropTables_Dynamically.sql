49bf992fe09a6f5022f78bf3c961f93b96749e2a
Farooq Zafar <53262137+farooqzafarawan@users.noreply.github.com>
2020-01-21 17:30:50 +0500

    Create DropTables_Dynamically.sql

---
+++ DropTables_Dynamically.sql
@@ -0,0 +1,12 @@
+--Drop Tables in DB using dynamic SQL Query
+
+--Disable Foreign Keys present in Database
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+
+DECLARE @sql NVARCHAR(max)=''
+
+SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
+FROM   INFORMATION_SCHEMA.TABLES
+WHERE  TABLE_TYPE = 'BASE TABLE'
+
+Exec Sp_executesql @sql
